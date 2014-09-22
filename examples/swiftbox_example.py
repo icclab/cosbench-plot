15a170db72195891bb6489617464183c2aa4cab4
Gabriel Ankeshian <anke@zhaw.ch>
2014-09-22 10:23:52 +0200

    inserted license, removed unused function

--- examples/swiftbox_example.py
+++ examples/swiftbox_example.py
@@ -1,34 +1,42 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'gank'
 
-import matplotlib
-from plotter.plotgenerator import StagePlotGenerator,\
-    WorkloadPlotGenerator, RTPlotGenerator
-from parser.StageFileParser import StageFileParser
+'''
+Copyright 2014 ZHAW (Zürcher Hochschule für Angewandte Wissenschaften)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+from plotter.plotgenerator import StagePlotGenerator
 
 BASE_INPUT_PATH = '/Users/gank/src/'
 BASE_OUTPUT = '/Users/gank/src/graphs/'
 
-swift_workload_ids = [20]
+workload_ids = [20]
 
-def createStageGraphsForEachStat():
-    outdir=BASE_OUTPUT + '/stage/'
-    plotgen = StagePlotGenerator(BASE_INPUT_PATH, outdir, '^s[0-9]+-')
-    plotgen.addWorkloadIds('swift', swift_workload_ids)
-    plotgen.createAllStagePlots(outdir)
-
-
-
+'''
+This module creates the charts to compare every workstage of a single workload.
+'''
 def createAggregatedGraphsForStages():
     lineStyleRule = lambda stname: '-'
     outdir = BASE_OUTPUT + '/throughput/'
-    plotgen = StagePlotGenerator(BASE_INPUT_PATH, outdir, 's.*')
-    plotgen.addWorkloadIds('swift', swift_workload_ids)
-
+    plotgen = StagePlotGenerator(BASE_INPUT_PATH, outdir)
+    plotgen.addWorkloadIds('swift', workload_ids)
     plotgen.setWorkloadFilter('w20-eagle')
     plotgen.setFilenameFilter('s[0-9]+.*$')
     plotgen.createAggregatedChart(outdir, 'w20_eagle' + "swiftbox", '(.*)', ['Throughputread'], lineStyleRule)
 
 
 if __name__ == '__main__':
-    print matplotlib.__version__
     createAggregatedGraphsForStages()
